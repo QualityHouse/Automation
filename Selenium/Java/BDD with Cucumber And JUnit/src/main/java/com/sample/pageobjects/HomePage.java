d0a3ff5d81d31202133ca09c6b347aa4389591ac
Zhivko Georgiev <zdgeorgiev@abv.bg>
2015-07-03 12:32:55 +0300

    Added sample project demonstrating implementation of the Page Objects pattern using Cucumber. Using JUnit as test framework.

---
+++ Selenium/Java/BDD with Cucumber And JUnit/src/main/java/com/sample/pageobjects/HomePage.java
@@ -0,0 +1,30 @@
+package com.sample.pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePageObject {
+	private final String expectedPageTitle = "Българският Интернет портал!";
+	
+	@CacheLookup
+	@FindBy(css = "#lenta-login a")
+	private WebElement loginButton;
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+	}
+	
+	public boolean verifyBasePageTitle() {
+		return getPageTitle().contains(expectedPageTitle);
+	}
+
+	public LoginPage clickLoginButton() {
+		waitForElementToBeDisplayed(loginButton);
+		loginButton.click();
+
+		return PageFactory.initElements(getDriver(), LoginPage.class);
+	}
+}
---
+++ Selenium/Java/BDD with Cucumber And JUnit/src/test/resources/features/login.feature
@@ -0,0 +1,17 @@
+Feature: Login
+
+	Scenario: Successful Login
+	Given the login page is opened
+	When I enter valid users's email in the email field
+	And I enter valid password in the password field
+	And I submit
+	Then my email should be displayed in the upper right corner
+	And I should be able to logout
+    
+	Scenario: Unsuccessful Login
+	Given the login page is opened
+	When I enter invalid users's email in the email field
+	And I enter invalid password in the password field
+	And I submit
+	Then an error text appears
+	And I'm not logged in
